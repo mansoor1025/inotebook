675d337794a5c32251c5bd7ce1ef47b587f9b9a3
Mansoor Ali <mansoor.ali@itsmstaging.net>
2022-02-15 22:23:42 -0800

    bcyrt salt jwt token

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,23 +1,51 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const jwt_secret = "dasdasdasdas";
 router.post('/add-user', [
     body('name', 'name is required').exists(),
     body('email').isEmail().exists(),
     body('password', 'password is required').exists()
 ], async (req, res,) => {
     try {
+        const saltRounds = 10;
+        let secure_password = '';
+
+        // check validation errors
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        var users = new User(req.body)
-        await users.save();
-        res.send('user add successfully');
+
+        // duplicate email validations
+        const user = await User.find({ email: req.body.email });
+        if (user.length > 0) {
+            res.status('400').send('Email Already Exists');
+        }
+
+        // create hash password with gensalt
+        await bcrypt.hash(req.body.password, saltRounds, async function (err, hash) {
+            const users = {
+                name: req.body.name,
+                email: req.body.email,
+                password: hash
+            }
+            await new User(users).save();
+
+            const user_data = await User.find({ email: req.body.email });
+            const data = {
+                user_id: {
+                    id: user_data[0]._id
+                }
+            }
+            const auth_token = await jwt.sign(data, jwt_secret);
+            res.json({ auth_token });
+            console.log(auth_token);
+        });
     } catch (error) {
-        console.log('error generating');
-        console.log(error);
         res.send(error)
     }
 })
